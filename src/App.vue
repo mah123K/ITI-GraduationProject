683caded6f3650597f31f5f6466c913a17e7cc35
Mahmoud Soliman <mah123khso@gmail.com>
2025-10-21 11:40:43 +0300

    added about us and profile page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
+<script>
+  import AboutUs from "./components/AboutUs.vue";
+import TechnichionProfile from './components/technichionProfile.vue';
+  export default 
+  {
+    name:"App",
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      AboutUs,
+      TechnichionProfile
+    }
+  }
+</script>
 <template>
   <div>
-
+    <AboutUs/>
+    <technichionProfile/>
   </div>
 </template>
 
-<script>
-  export default {
-    
-  }
-</script>
 
 <style scoped>
 
