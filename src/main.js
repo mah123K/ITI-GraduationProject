2ea4bff3b84415cf52edee4342ad1c1e41200e1e
Mahmoud Soliman <mah123khso@gmail.com>
2025-10-22 00:18:51 +0300

    latest update

--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
 import "./assets/main.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./components/HomePage.vue";
 import OfferPage from "./components/OfferPage.vue";
 import LoginPage from "./components/LoginPage.vue";
-
+import SignUp from "./components/SignUp.vue";
+import CarpentersPage from "./components/CarpentersPage.vue";
+import ElectrectionsPage from "./components/ElectrectionsPage.vue";
+import FinishingCompanies from "./components/FinishingCompanies.vue";
+import technichionProfile from "./components/technichionProfile.vue";
+import ContactUs from "./components/ContactUs.vue";
 import App from "./App.vue";
 import AboutUs from "./components/AboutUs.vue";
-import ProfilesPage from "./components/profilesPage.vue";
+import PlumbersPage from "./components/PlumbersPage.vue";
 const routes = [
   { path: "/", component: HomePage },
   { path: "/offers", component: OfferPage },
   { path: "/login", component: LoginPage },
+    { path: "/SignUp", component: SignUp },
   { path: "/about", component: AboutUs },
-  { path: "/profiles", component: ProfilesPage },
+  { path: "/PlumbersPage", component: PlumbersPage },
+  { path: "/CarpentersPage", component: CarpentersPage },
+  { path: "/ElectrectionsPage", component: ElectrectionsPage },
+  { path: "/FinishingCompanies", component: FinishingCompanies },
+  { path: "/technichionProfile", component: technichionProfile },
+  { path: "/ContactUs", component: ContactUs },
 ];
 const router = createRouter({ history: createWebHistory(), routes });
 
