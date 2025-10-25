0948c6bb6e7259969f4df2724cf275191d1b8291
Mahmoud Soliman <mah123khso@gmail.com>
2025-10-25 15:48:17 +0300

    minor update

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ import ServiceCard from "./components/ServiceCard.vue";
 
 // ✅ Firebase Config
 const firebaseConfig = {
- apiKey: "AIzaSyCoEkOce-LY7cXvtJHzvyVaygMAjPIzU3k",
+  apiKey: "AIzaSyCoEkOce-LY7cXvtJHzvyVaygMAjPIzU3k",
   authDomain: "tashteb-36a40.firebaseapp.com",
   projectId: "tashteb-36a40",
   storageBucket: "tashteb-36a40.firebasestorage.app",
@@ -82,6 +82,17 @@ const routes = [
       { path: "payments", name: "Payments", component: Payments },
       { path: "support", name: "Support", component: Support },
       { path: "settings", name: "Settings", component: Settings },
+    ],
+  },
+    {
+    path: "/technician-dashboard",
+    component: TechncionDashboard,
+    meta: { requiresTechnician: true },
+    children: [
+      { path: "", name: "TechnicianOrders", component: ordersCard },
+      { path: "services", name: "TechnicianServices", component: ServiceCard },
+      { path: "appointments", name: "TechnicianAppointments", component: MyAppointments },
+      { path: "create-service", name: "CreateServiceCard", component: CreateServiceCard },
     ],
   },
 ];
