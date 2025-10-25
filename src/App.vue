169c4494e4003ae3ec4c10a21063fc80ba71e085
rehab <rehabmahmoud0113@gmail.com>
2025-10-25 14:58:03 +0300

    Remove nav&footer from some pages

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <script>
-  import FooterLayout from './layout/FooterLayout.vue';
-  import NavBar from './layout/NavBar.vue'
-  import TechncionDashboard from './components/TechncionDashboard.vue';
-  import TechnicionDashNav from './layout/TechnicionDashNav.vue';
-  import MannageUserProfile from './components/MannageUserProfile.vue';
+import FooterLayout from "./layout/FooterLayout.vue";
+import NavBar from "./layout/NavBar.vue";
+import TechncionDashboard from "./components/TechncionDashboard.vue";
+import TechnicionDashNav from "./layout/TechnicionDashNav.vue";
+import MannageUserProfile from "./components/MannageUserProfile.vue";
 
-  export default 
-  {
-    name:"App",
-    data(){
-      return{
-
-      }
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  computed: {
+    hideLayout() {
+      const hiddenRoutes = ["/login", "/signup", "/dashboard"];
+      return hiddenRoutes.some((path) => this.$route.path.startsWith(path));
     },
-    components:{
-      NavBar,
-      TechncionDashboard,
-      FooterLayout,
-      TechnicionDashNav,
-      MannageUserProfile
-    }
-  }
+  },
+  components: {
+    NavBar,
+    TechncionDashboard,
+    FooterLayout,
+    TechnicionDashNav,
+    MannageUserProfile,
+  },
+};
 </script>
 <template>
-  <!-- <nav-bar/>
-  <router-view/>
-  <footer-layout/>  -->
-  <TechnicionDashNav/>
-  <TechncionDashboard />
+  <nav-bar v-if="!hideLayout" />
+  <router-view />
+  <footer-layout v-if="!hideLayout" />
+  <!-- <TechnicionDashNav/>
+  <TechncionDashboard /> -->
   <!-- <MannageUserProfile/> -->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
