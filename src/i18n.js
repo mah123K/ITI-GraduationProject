e6c7a676f6bb41880a6abd5328fcd269a08b8da3
Rania Ashraf <raniaashraf3030@gmail.com>
2025-11-08 16:45:37 +0200

    new update

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import { createI18n } from 'vue-i18n';
 
 // Import your message files
-import enMessages from './locals/en.json';
-import arMessages from './locals/ar.json';
+import enMessages from '../public/locals/en.json';
+import arMessages from '../public/locals/ar.json';
 
 // Get the saved language from localStorage, or set a default
 const savedLang = localStorage.getItem('lang') || 'ar'; // Default to Arabic
